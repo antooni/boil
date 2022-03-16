4816a5f61d6d9106924609e70a5a15008cd2675a
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-03-16 10:54:31 +0100

    Add buildGraph method to CPM

--- packages/core/src/services/CPM.ts
+++ packages/core/src/services/CPM.ts
@@ -7,69 +7,65 @@ interface Boundary {
 }
 
 export class CPM {
-  private graph: Map<string, Boundary> = new Map();
-
-  add(a: number, b: number): number {
-    return a + b;
-  }
-
-  buildGraph(activities: Input[]): Map<string, { from: number; to: number }> {
+  //TODO function to get final index
+  buildGraph(
+    activities: Input[],
+    finalIndex: number
+  ): Map<string, { from: number; to: number }> {
     const graph = new Map();
 
     let lastIndex = 1;
 
-    for (const [index, activity] of activities.entries()) {
-      if (activity.previous.length === 0) {
-        graph.set(`${activity.activity}`, { from: 1, to: ++lastIndex });
-      } else {
-        for (const pp of activity.previous) {
-          const known = activities
-            .slice(index)
-            .map((a) => a.previous)
-            .flat();
+    for (const activity of activities) {
+      const from = this.findFrom(activity, graph);
+      const to = this.findTo(
+        activity,
+        activities,
+        graph,
+        lastIndex,
+        finalIndex
+      );
+      if (to > lastIndex) lastIndex++;
 
-          if (known.includes(activity.activity)) {
-            for (const aa of activities) {
-              if (
-                aa.previous.includes(activity.activity) &&
-                aa.previous.length > 1
-              ) {
-                const nn: Boundary = graph.get(aa.activity);
-                const prevNode: Boundary = graph.get(pp);
-                graph.set(`${activity.activity}`, {
-                  from: prevNode.to,
-                  to: nn.to,
-                });
-              }
-            }
-          } else {
-            const node: Boundary = graph.get(prev);
-            graph.set(`${activity.activity}`, {
-              from: node.to,
-              to: ++lastIndex,
-            });
-          }
-        }
-
-        return graph;
-      }
+      graph.set(`${activity.activity}`, { from, to });
     }
 
-    return graph
+    return graph;
   }
 
-  nextNodes(nodeNumber: number, activities: Input[]): Node[] {
-    const result: Node[] = [];
-    let nextIndex = nodeNumber;
-    for (const activity of activities) {
-      if (activity.previous.length === 0) {
-        result.push({ num: ++nextIndex, next: [] });
+  findFrom(activity: Input, graph: Map<string, Boundary>): number {
+    if (activity.previous.length === 0) {
+      return 1;
+    } else {
+      const predecessor = graph.get(activity.previous[0]);
+      return predecessor ? predecessor?.to : -1;
+    }
+  }
+
+  findTo(
+    activity: Input,
+    activities: Input[],
+    graph: Map<string, Boundary>,
+    lastIndex: number,
+    finalIndex: number
+  ): number {
+    if (lastIndex >= finalIndex) {
+      return finalIndex;
+    }
+    if (activity.previous.length === 0) {
+      return ++lastIndex;
+    } else {
+      const index = activities.indexOf(activity);
+      const known = activities
+        .slice(index)
+        .find((a) => a.previous.includes(activity.activity));
+      if (known === undefined || known.previous.length === 1) {
+        return ++lastIndex;
       } else {
-        for (const prev of activity.previous) {
-        }
+        const brother = known?.previous[0];
+        const predecessor = graph.get(brother);
+        return predecessor ? predecessor?.to : ++lastIndex;
       }
     }
-
-    return result;
   }
 }
--- packages/core/test/services/CPM.test.ts
+++ packages/core/test/services/CPM.test.ts
@@ -45,43 +45,99 @@ const ACTIVITIES: Input[] = [
     previous: ["C", "D"],
     duration: 5,
   },
+  {
+    activity: "I",
+    previous: ["C", "D"],
+    duration: 4,
+  },
+  {
+    activity: "J",
+    previous: ["E"],
+    duration: 3,
+  },
+  {
+    activity: "K",
+    previous: ["F", "G", "H"],
+    duration: 6,
+  },
+  {
+    activity: "L",
+    previous: ["F", "G", "H"],
+    duration: 3,
+  },
+  {
+    activity: "M",
+    previous: ["I"],
+    duration: 6,
+  },
+  {
+    activity: "N",
+    previous: ["J", "K"],
+    duration: 9,
+  },
 ];
 
-describe(CPM.prototype.nextNodes.name, () => {
-  it("1 has 2,3,4", () => {
-    expect(cpm.nextNodes(1, ACTIVITIES.slice(0, 3))).toEqual([
-      { num: 2, next: [] },
-      { num: 3, next: [] },
-      { num: 4, next: [] },
-    ]);
-  });
-});
+const GRAPH = new Map([
+  ["A", { from: 1, to: 2 }],
+  ["B", { from: 1, to: 3 }],
+  ["C", { from: 1, to: 4 }],
+  ["D", { from: 3, to: 4 }],
+  ["E", { from: 2, to: 5 }],
+  ["F", { from: 2, to: 6 }],
+  ["G", { from: 3, to: 6 }],
+  ["H", { from: 4, to: 6 }],
+  ["I", { from: 4, to: 7 }],
+  ["J", { from: 5, to: 8 }],
+  ["K", { from: 6, to: 8 }],
+  ["L", { from: 6, to: 9 }],
+  ["M", { from: 7, to: 9 }],
+  ["N", { from: 8, to: 9 }],
+]);
 
 describe(CPM.prototype.buildGraph.name, () => {
   it("ABC", () => {
-    expect(cpm.buildGraph(ACTIVITIES.slice(0, 3))).toEqual(
+    expect(cpm.buildGraph(ACTIVITIES.slice(0, 3), 9)).toEqual(
       new Map([
-        ["A", {from: 1, to: 2}],
-        ["B", {from: 1, to: 3}],
-        ["C", {from: 1, to: 4}],
+        ["A", { from: 1, to: 2 }],
+        ["B", { from: 1, to: 3 }],
+        ["C", { from: 1, to: 4 }],
       ])
     );
   });
 
   it("ABCD", () => {
-    expect(cpm.buildGraph(ACTIVITIES)).toEqual(
-      new Map([
-        ["A", {from: 1, to: 2}],
-        ["B", {from: 1, to: 3}],
-        ["C", {from: 1, to: 4}],
-        ["D", {from: 3, to: 4}],
-        // ["E", {from: 2, to: 5}],
-        // ["F", {from: 2, to: 6}],
-        // ["G", {from: 3, to: 6}],
-        // ["H", {from: 4, to: 6}],
-      ])
-    );
+    expect(cpm.buildGraph(ACTIVITIES, 9)).toEqual(GRAPH);
+  });
+});
+
+describe(CPM.prototype.findFrom.name, () => {
+  it("A", () => {
+    expect(cpm.findFrom(ACTIVITIES[0], GRAPH)).toEqual(1);
   });
 
-  
+  it("D", () => {
+    expect(cpm.findFrom(ACTIVITIES[3], GRAPH)).toEqual(3);
+  });
+
+  it("H", () => {
+    expect(cpm.findFrom(ACTIVITIES[7], GRAPH)).toEqual(4);
+  });
+});
+
+describe(CPM.prototype.findTo.name, () => {
+  it("A", () => {
+    expect(cpm.findTo(ACTIVITIES[0], ACTIVITIES, GRAPH, 1, 9)).toEqual(2);
+  });
+
+  it("E", () => {
+    expect(cpm.findTo(ACTIVITIES[4], ACTIVITIES, GRAPH, 4, 9)).toEqual(5);
+  });
+
+  it("D", () => {
+    expect(cpm.findTo(ACTIVITIES[3], ACTIVITIES, GRAPH, 4, 9)).toEqual(4);
+  });
+
+  it("M", () => {
+    expect(cpm.findTo(ACTIVITIES[12], ACTIVITIES, GRAPH, 9, 9)).toEqual(9);
+  });
 });
